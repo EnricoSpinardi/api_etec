87786182d6920421b6b7d6d54caa2c70ca6a8859
Enrico  Spinardi <167817620+EnricoSpinardi@users.noreply.github.com>
2026-05-12 15:05:15 -0300

    npm install express (Está sem node modules)

---
+++ server.js
@@ -0,0 +1,66 @@
+// Enrico A. Spinardi 
+
+//importa express
+const express = require('express');
+const app = express();
+
+//cria rota GET - Principal
+app.get('/', (req, res) =>{
+    //retorna json
+    res.json({
+        mensagem: 'API Funcionando',
+        curso: 'ETEC DS'
+    });
+
+});
+
+app.get('/usuarios', (req, res) => {
+    const usuarios = [
+        {
+            id: 1,
+            nome: 'André',
+            idade: 23,
+            email: 'andre@gmail.com',
+            cidade: 'Sorocaba'
+        },
+
+        {
+            id: 2,
+            nome: 'Maria',
+            idade: 19,
+            email: 'maria@gmail.com',
+            cidade: 'Salto'
+        },
+
+        {
+            id: 3,
+            nome: 'Pedro',
+            idade: 45,
+            email: 'pedro@gmail.com',
+            cidade: 'São Paulo'
+        },
+
+        {
+            id: 4,
+            nome: 'Lucas',
+            idade: 20,
+            email: 'lucas@gmail.com',
+            cidade: 'Itu'
+        }
+    ];
+
+    res.json(usuarios);
+});
+
+/*
+//cria rota GET
+app.get('/', (req, res)=> {
+    //envia a resposta
+    res.send("API funcionado!");
+}); 
+*/
+
+//Inicia servidor
+app.listen(3000, () => {
+    console.log("Servidor Funcionado");
+})
