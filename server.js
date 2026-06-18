0e59a93122b3bf28a19c116d9636522f327d8cf3
EnricoSpinardi <easpinardi@gmail.com>
2026-06-17 23:45:17 -0300

    Personalização Bootstrap

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static ('.'));
 
 //conexão mysql
 const conexao = mysql.createConnection({
